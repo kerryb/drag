386530f20038bcf534af9327b370d59b875eed85
Kerry Buckley <kerryjbuckley@gmail.com>
2023-06-26 20:51:38 +0100

    Simulate opponent playing a random card

--- lib/drag_web/live/home_live.ex
+++ lib/drag_web/live/home_live.ex
@@ -11,17 +11,28 @@ defmodule DragWeb.HomeLive do
   @impl LiveView
   def mount(_params, _session, socket) do
     cards = Enum.into(1..4, %{}, &{&1, %Card{id: &1}})
-    {:ok, assign(socket, cards_in_hand: cards, cards_on_table: [])}
+    opponent_cards = Enum.into(5..8, %{}, &{&1, %Card{id: &1}})
+
+    {:ok,
+     assign(socket,
+       cards_in_hand: cards,
+       cards_on_table: [],
+       opponent_cards_in_hand: opponent_cards,
+       opponent_cards_on_table: []
+     )}
   end
 
   @impl LiveView
   def handle_event("drop", %{"id" => id}, socket) do
     {card, cards} = Map.pop(socket.assigns.cards_in_hand, String.to_integer(id))
+    id = socket.assigns.opponent_cards_in_hand |> Map.keys() |> Enum.random()
+    {opponent_card, opponent_cards} = Map.pop(socket.assigns.opponent_cards_in_hand, id)
 
     {:noreply,
      socket
-     |> assign(cards_in_hand: cards)
-     |> update(:cards_on_table, &[card | &1])}
+     |> assign(cards_in_hand: cards, opponent_cards_in_hand: opponent_cards)
+     |> update(:cards_on_table, &[card | &1])
+     |> update(:opponent_cards_on_table, &[opponent_card | &1])}
   end
 
   defp offset(0), do: "top-[10px] left-[10px]"
